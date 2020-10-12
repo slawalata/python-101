b424ef7f42b0f80b7c082a6ad92f8b81d37be8a1
sanga lawalata <sanga.lawalata@gmail.com>
2020-10-12 13:27:59 +0700

    add example

--- oop/awarenes_onetomany.py
+++ oop/awarenes_onetomany.py
@@ -1,25 +1,25 @@
-# Library 1 -----> 0..* NewspaperShelve
-# Library has (and aware of) Newspaper Shelve
+# Library 1 -----> 1 NewspaperShelve ----> 0..20 Newspaper.
+# Library has (and aware of) Newspaper Shelve and NP shelves has newspaper.
 # pictured with an arrow
 
+class Newspaper:
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+
+
 class NewsPaperShelve:
-    def __init__(self, id):
+    def __init__(self, id, capacity):
         self.id = id
+        self.capacity = capacity
+        self.newspapers = [None] * self.capacity
 
 
 class Library:
-    def __init__(self):
-        self.newsPaperShelves = []
-
-    def add(self, shelves):
-        self.newsPaperShelves.append(shelves)
+    def __init__(self, newspaper_shelve):
+        self.newspaper_shelve = newspaper_shelve
 
 
 if __name__ == '__main__':
-    library = Library()
-
-    kompasShelves = NewsPaperShelve("KMP")
-    suaraPembaruanShelve = NewsPaperShelve("SP")
-
-    library.add(kompasShelves)
-    library.add(kompasShelves)
+    all_newspaper_shelve = NewsPaperShelve(1, 20)
+    my_library = Library(all_newspaper_shelve)
