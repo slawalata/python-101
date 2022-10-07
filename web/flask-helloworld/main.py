0bcb706d439ef5e60db33c04e73d7dc89a2a6631
slawalata <sanga.lawalata@gmail.com>
2022-10-07 12:31:45 +0700

    add flask

---
+++ web/flask-helloworld/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, app
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=81, debug=True)
